7641223c6bae4d7605cd01f3b9f4988b2d42df98
vlad moisei <vladmoisei@yahoo.com>
2020-06-04 16:59:06 +0100

    Add Index and Consum Model

---
+++ MVCWithBlazor/Models/ConsumEnergieModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWithBlazor.Models
+{
+    public class ConsumEnergieModel
+    {
+        public int ConsumEnergieModelID { get; set; }
+        public DateTime Data { get; set; }
+        public int Ora { get; set; }
+        public float Valoare { get; set; }
+    }
+}
---
+++ MVCWithBlazor/Models/IndexModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWithBlazor.Models
+{
+    public class IndexModel
+    {
+        public int IndexModelID { get; set; }
+
+        public DateTime DataOra { get; set; }
+
+        public int EdisStatus { get; set; }
+
+        public float EnergyPlusA { get; set; }
+
+        public float EnergyMinusA { get; set; }
+
+        public float EnergyPlusRi { get; set; }
+
+        public float EnergyPlusRc { get; set; }
+
+        public float EnergyMinusRi { get; set; }
+
+        public float EnergyMinusRc { get; set; }
+    }
+}
